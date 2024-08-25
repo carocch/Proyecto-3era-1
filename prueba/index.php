04f8910fefa276faf8711b0af28c8bab76507a8d
Carolina <carol.cch97@gmail.com>
2024-08-25 10:34:28 -0600

    Prueba de proyecto

---
+++ prueba/index.php
@@ -0,0 +1,29 @@
+<?php require_once "./parte_superior.php"?>
+<div class="titulo">
+   <p>Astica</p> 
+</div>
+
+<body>
+    <div class="main-img">
+    <img src="./Images/Clases-en-linea-1.png" alt="">
+    </div>
+   
+     
+    
+   
+    <div class="main-accordion-container">
+    </div>
+
+
+    <div class="mid-info">
+    <p class="mid-info-tittle">Inscripciones Abiertas</p>
+    <p class="mid-info-desc">Club de tiro con Arco</p>
+</div>
+
+<div class="card-list">
+
+</div>
+
+</body>
+
+<?php require_once "./parte_inferior.php"?>
